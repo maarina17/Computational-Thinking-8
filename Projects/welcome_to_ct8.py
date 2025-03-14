0ca29f2c18aacd56ebd4f07b13b14265b9b93a21
Osae-ux <oscarforman@seattleacademy.org>
2025-03-14 15:35:59 +0000

    Image practice program

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -6,16 +6,15 @@ stage = StageClass()
 ###############################################
 
 
-stage.set_background("spring")
-mySprite = codesters.Sprite("cardinal")
-mySprite.say("Good job finding me!")
-
+stage.set_background("latham")
+mySprite = codesters.Sprite("dillon")
+mySprite.say("sea salt spray",color="green")
+mySprite.set_size(1)
 
 print("Welcome to CT8!")
 print("Great job correctly setting up this codespace")
 print("We will be using this site for the majority of the projects for this class")
 print("Make sure you bookmark this page using the STAR icon in the top right corner of your browser")
-input("\n---Press Enter when you have bookmarked the page---\n")
 
 
 print("\n\n")
