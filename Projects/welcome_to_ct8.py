0043d68a88787d5fbfcd2936e9b7c56f15c7188f
CoolCats265 <ayamestevens@seattleacademy.org>
2025-03-14 18:21:29 +0000

    setup

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -6,16 +6,16 @@ stage = StageClass()
 ###############################################
 
 
-stage.set_background("spring")
-mySprite = codesters.Sprite("cardinal")
-mySprite.say("Good job finding me!")
+stage.set_background("winter")
+mySprite = codesters.Sprite("butterfly")
+mySprite.set_size(0.25)
+mySprite.say("You are fun")
 
 
 print("Welcome to CT8!")
 print("Great job correctly setting up this codespace")
 print("We will be using this site for the majority of the projects for this class")
 print("Make sure you bookmark this page using the STAR icon in the top right corner of your browser")
-input("\n---Press Enter when you have bookmarked the page---\n")
 
 
 print("\n\n")
@@ -26,3 +26,7 @@ print("\t2: move your mouse over the words in blue that start with https://")
 print("\t3: a few icons should appear - click the globe")
 print("\t4: a new tab will open - click CONNECT")
 print("\n\nWhen you have found the CARDINAL, click here, then use CTRL C to end the program\n\n")
+mySprite2 = codesters.Sprite("prettyrainbow",-200,200)
+mySprite2.set_size(0.9)
+mySprite3 = codesters.Sprite("tropicalflowers",150,-100)
+mySprite3.set_size(0.6)
